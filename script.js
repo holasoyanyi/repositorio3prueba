25a89520a8aeba548885faa17888336229fdd625
holasoyanyi <152664488+holasoyanyi@users.noreply.github.com>
2023-12-12 20:30:57 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function encriptar() {
+passSistema = "jelouyisus1234"
+msgError= "Error en la contraseña, vuelve a intentarlo";
+var mensaje = document.getElementById("msg").value;
+var password = document.getElementById("pass").value;
+alert(mensaje+"+"+password)
+
+
+}
+
+function desencriptar() {
+
+
+
+}
